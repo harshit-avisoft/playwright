ed663f7d139d0f27beb7f6b6d46c9d6cbaac52e1
Harshit <Harshit.pandey@avisoft.io>
2025-12-23 11:16:03 +0530

    Delete mini_project1 directory

--- mini_project1/utils/crypto_utils.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = b'G5EmOTK3hCUMYT_xBzwQWbZO3jx-avQN23BgDWzeLrA='
-
-# Use plain text credentials (simplifies testing)
-# If you need encryption, encrypt them first with Fernet(key).encrypt(b'value')
-user_name = "standard_user"
-password = "secret_sauce"
--- mini_project1/utils/items.py
+++
@@ -1,7 +0,0 @@
-class InventoryItems:
-    BIKE_LIGHT = "Sauce Labs Bike Light"
-    BACKPACK = "Sauce Labs Backpack"
-    BOLT_TSHIRT = "Sauce Labs Bolt T-Shirt"
-    FLEECE_JACKET = "Sauce Labs Fleece Jacket"
-    Onesie="Sauce Labs Onesie"
-    Red_T_Shirt="Test.allTheThings() T-Shirt (Red)"
